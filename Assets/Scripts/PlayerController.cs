ac028993c35b1a5cef73143c8410ec4015cf213f
kk <wanqingkan1996@foxmail.com>
2020-05-05 14:34:13 +0200

    original version

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Transform _transform;
+    Camera _camera;
+
+
+    void Start()
+    {
+        this._transform = transform;
+        this._camera = Camera.main;
+    }
+
+    //Standard UpdateLoop (once per Frame)
+    void Update()
+    {
+        this.Rotate();
+    }
+
+    void Rotate(){
+        Vector2 mousePos = this._camera.ScreenToWorldPoint(Input.mousePosition);
+        float angleRad = Mathf.Atan2(mousePos.y - this._transform.position.y, mousePos.x - this._transform.position.x);
+        float angleDeg = (180 / Mathf.PI) * angleRad;
+        this._transform.rotation = Quaternion.Euler(0, 0, angleDeg - 90);//diese -90 sind nötig für Sprites, die nach oben zeigen. Nutzen Sie andere Assets, könnte es sein, dass die das anpassen müssen
+        
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Vector3 _direction;
+    Transform _transform;
+    Camera _camera;
+    
+    //We set values in a Init method. Virtual, so we can extend it later :)
+    public virtual Projectile Init(Vector3 direction){
+        this._direction = direction;
+        /*
+        Your Code here
+        */
+        return this;
+    }
+
+    void Start()
+    {
+        this._transform = this.transform;
+        this._camera = Camera.main;
+        this.Rotate();
+    }
+
+    void Update()
+    {
+
+    }
+
+    
+    void Rotate(){
+        Vector3 pos = this._transform.position + this._direction;
+        float AngleRad = Mathf.Atan2(pos.y - this._transform.position.y, pos.x - this._transform.position.x);
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        this._transform.rotation = Quaternion.Euler(0, 0, AngleDeg - 90);
+    }
+}
