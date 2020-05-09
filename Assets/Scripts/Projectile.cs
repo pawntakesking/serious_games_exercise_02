f157775ab30a85afe9be6757d661f1f5221359dd
KingZ <50152613+KingZ23@users.noreply.github.com>
2020-05-09 23:08:41 +0200

    Update Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,14 +8,14 @@ public class Projectile : MonoBehaviour
     Transform _transform;
     Camera _camera;
     //自毁计时器
-    public float _timer=5;
+    /*public float _timer=5;*/ //放到Controller那边去了 -包
     public float _speed=1;
     
     
     //We set values in a Init method. Virtual, so we can extend it later :)
     public virtual Projectile Init(Vector3 direction){
         this._direction = direction;
-        Destroy(this,_timer);//我不确定放这里还是start //貌似都ok -包
+        /*Destroy(this,_timer);*///我不确定放这里还是start //我写到PlayerController那边去了，这样在运行的时候可以看到那些GameObject都被删掉了 -包
         /*
         Your Code here
         */
@@ -29,8 +29,8 @@ public class Projectile : MonoBehaviour
         this._transform = this.transform;
         this._camera = Camera.main;
         this.Rotate();
-        Vector _mousePos = new Vector3(this._camera.ScreenToWorldPoint(Input.mousePosition).x, 
-                                       this._camera.ScreenToWorldPoint(Input.mousePosition).y, 0); // -包
+        Vector _mousePos = new Vector3(this._camera.ScreenToWorldPoint(Input.mousePosition).x - this._transform.position.x, 
+                                       this._camera.ScreenToWorldPoint(Input.mousePosition).y - this._transform.position.y, 0); // -包
     }
 
     void Update()
