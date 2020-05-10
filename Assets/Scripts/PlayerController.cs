3fce1d51d609494ddf27844b4b2e5994c4ce276f
KingZ <50152613+KingZ23@users.noreply.github.com>
2020-05-10 14:36:52 +0200

    Add files via upload

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,58 +1,41 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public float _speed;
+    
+    public GameObject _prefab;
     Transform _transform;
     Camera _camera;
-    public float speed =2;
-    float x,y;
-    /*public float _speed;*/
-    public GameObject _prefab; //-包
-    float _cd = 3; //冷却时间，暂定3秒
-    float _ready = 0;//冷却完成，可以发射 
-    float _destroyTime = 5f; //毁灭时间 -包
+    float _destroyTime = 5f;
+    float _cd = 1f;
+    float _ready = 0f;
+
     
 
     void Start()
     {
         this._transform = transform;
         this._camera = Camera.main;
+        
     }
 
     //Standard UpdateLoop (once per Frame)
     void Update()
     {
-        this.Rotate();
-        //目前不知道wasd方向是哪个对应xyz，所以先这样写，之后确定了再修改
-        if(Input.GetKey(KeyCode.W))
-        {
-            y=speed*Time.deltaTime;
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            y=-speed*Time.deltaTime;
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            x=speed*Time.deltaTime;
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            x=-speed*Time.deltaTime;
-        }
-
-        transform.Translate(new Vector3(x,y,0),Space.World);
-        //这样应该可以了
-        
-        /*this.move();*/ 
-         if(Input.GetMouseButtonDown(0) && Time.time > _ready){
+        this.Rotate();   
+        this.move();
+        if(Input.GetMouseButtonDown(0) && Time.time > _ready){
             Debug.Log("Left Mouse clicked");
-            _ready = Time.time + _cd; //每_cd秒能够发射一次
+            _ready = Time.time + _cd;
+            Debug.Log("Time added");
             GameObject clone = Instantiate(_prefab, this._transform.position, Quaternion.identity) as GameObject;
-            Destroy(clone, this._destroyTime); //得写在这里才会删掉
-            } //左键的时候发射激光 -包   
+            Destroy(clone, this._destroyTime);
+        }
     }
+    
+    
 
     void Rotate(){
         Vector2 mousePos = this._camera.ScreenToWorldPoint(Input.mousePosition);
@@ -61,16 +44,19 @@ public class PlayerController : MonoBehaviour
         this._transform.rotation = Quaternion.Euler(0, 0, angleDeg - 90);//diese -90 sind nötig für Sprites, die nach oben zeigen. Nutzen Sie andere Assets, könnte es sein, dass die das anpassen müssen
         
     }
-     //这是我的版本，上传上来看看
-     /*void move(){
-       
-        float _horizontalInput = Input.GetAxis("Horizontal");
-        
-        float _verticalInput = Input.GetAxis("Vertical");
-       
-        _transform.position = _transform.position + new Vector3(_horizontalInput * _speed * Time.deltaTime, _verticalInput * _speed * Time.deltaTime, 0);
-      
+
+
+    void move(){
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+        //update the position
+        _transform.position = _transform.position + new Vector3(horizontalInput * _speed * Time.deltaTime, verticalInput * _speed * Time.deltaTime, 0);
+        //output to log the position change
         Debug.Log(_transform.position);
-    }*/ //-包
+    }
+
+    
 
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,44 +1,48 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
-{
-    Vector3 _direction;//鼠标方向
+{ 
+    
+    Vector3 _direction;
     Transform _transform;
     Camera _camera;
-    //自毁计时器
-    /*public float _timer=5;*/ //放到Controller那边去了 -包
+    Vector3 _mousePos;
     float _speed = 5f;
     
+
     
     //We set values in a Init method. Virtual, so we can extend it later :)
     public virtual Projectile Init(Vector3 direction){
         this._direction = direction;
-        /*Destroy(this,_timer);*///我不确定放这里还是start //我写到PlayerController那边去了，这样在运行的时候可以看到那些GameObject都被删掉了 -包
         /*
         Your Code here
         */
-        //normalized 使所有激光的飞行速度保持一致 -包
-        this.transform.position +=  (this._direction.normalized) * _speed  * Time.deltaTime; // -包
+
+        this.transform.position +=  (this._direction.normalized) * _speed  * Time.deltaTime;
+        
         return this;
     }
 
     void Start()
     {
-        
+        //_prefab = Assets.Prefabs.Load("GameObject") as GameObject;
         this._transform = this.transform;
+        Debug.Log("lol");
         this._camera = Camera.main;
         this.Rotate();
-        Vector _mousePos = new Vector3(this._camera.ScreenToWorldPoint(Input.mousePosition).x - this._transform.position.x, 
-                                       this._camera.ScreenToWorldPoint(Input.mousePosition).y - this._transform.position.y, 0); // -包
+        _mousePos = new Vector3(this._camera.ScreenToWorldPoint(Input.mousePosition).x - this._transform.position.x, this._camera.ScreenToWorldPoint(Input.mousePosition).y - this._transform.position.y,0);
+        
     }
 
     void Update()
     {
-        _transform.Translate(_direction*Time.deltaTime*_speed); //这一行是做什么的？
-        this.Rotate(); //不知道为什么，反正我这里还需要Rotate，不然我的Projectile就很怪 -包
-        this.Init(_mousePos); // -包
+        this.Rotate();
+        this.Init(_mousePos);
+        
+        
+
     }
 
     
@@ -46,6 +50,6 @@ public class Projectile : MonoBehaviour
         Vector3 pos = this._transform.position + this._direction;
         float AngleRad = Mathf.Atan2(pos.y - this._transform.position.y, pos.x - this._transform.position.x);
         float AngleDeg = (180 / Mathf.PI) * AngleRad;
-        this._transform.rotation = Quaternion.Euler(0, 0, AngleDeg - 90);
+        this._transform.rotation = Quaternion.Euler(0, 0, AngleDeg -90);
     }
 }
