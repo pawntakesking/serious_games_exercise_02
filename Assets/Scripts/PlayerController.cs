8655dc3859ee785786361aa9e8e57d944e137ff8
kk <wanqingkan1996@foxmail.com>
2020-05-10 16:53:11 +0200

    test new branch from kk

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,29 +1,55 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    public float _speed;
 
+    public GameObject _prefab;
     Transform _transform;
     Camera _camera;
-
+    float _destroyTime = 5f;
+    float _cd = 1f;
+    float _ready = 0f;
 
     void Start()
     {
         this._transform = transform;
         this._camera = Camera.main;
+
     }
 
     //Standard UpdateLoop (once per Frame)
     void Update()
     {
         this.Rotate();
+        this.move();
+        if (Input.GetMouseButtonDown(0) && Time.time > _ready)
+        {
+            _ready = Time.time + _cd;
+            GameObject clone = Instantiate(_prefab, this._transform.position, Quaternion.identity) as GameObject;
+            Destroy(clone, this._destroyTime);
+        }
     }
 
-    void Rotate(){
+    void Rotate()
+    {
         Vector2 mousePos = this._camera.ScreenToWorldPoint(Input.mousePosition);
         float angleRad = Mathf.Atan2(mousePos.y - this._transform.position.y, mousePos.x - this._transform.position.x);
         float angleDeg = (180 / Mathf.PI) * angleRad;
         this._transform.rotation = Quaternion.Euler(0, 0, angleDeg - 90);//diese -90 sind nötig für Sprites, die nach oben zeigen. Nutzen Sie andere Assets, könnte es sein, dass die das anpassen müssen
-        
     }
+
+
+    void move()
+    {
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+        //update the position
+        _transform.position = _transform.position + new Vector3(horizontalInput * _speed * Time.deltaTime, verticalInput * _speed * Time.deltaTime, 0);
+        //output to log the position change
+    }
+
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+
     Vector3 _direction;
     Transform _transform;
     Camera _camera;
-    
+    Vector3 _mousePos;
+    float _speed = 5f;
+
+
+
     //We set values in a Init method. Virtual, so we can extend it later :)
-    public virtual Projectile Init(Vector3 direction){
+    public virtual Projectile Init(Vector3 direction)
+    {
         this._direction = direction;
         /*
         Your Code here
@@ -19,18 +25,24 @@ public class Projectile : MonoBehaviour
 
     void Start()
     {
+        //_prefab = Assets.Prefabs.Load("GameObject") as GameObject;
         this._transform = this.transform;
         this._camera = Camera.main;
         this.Rotate();
+        _mousePos = new Vector3(this._camera.ScreenToWorldPoint(Input.mousePosition).x - this._transform.position.x, this._camera.ScreenToWorldPoint(Input.mousePosition).y - this._transform.position.y, 0);
+        this.Init(_mousePos);
+
     }
 
     void Update()
     {
-
+        this.Rotate();
+        this.transform.position += (this._direction.normalized) * _speed * Time.deltaTime;
     }
 
-    
-    void Rotate(){
+
+    void Rotate()
+    {
         Vector3 pos = this._transform.position + this._direction;
         float AngleRad = Mathf.Atan2(pos.y - this._transform.position.y, pos.x - this._transform.position.x);
         float AngleDeg = (180 / Mathf.PI) * AngleRad;
